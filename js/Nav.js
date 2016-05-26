70b25d641a173cb44d1f5f8061a5def7c4a15f51
hongee <hongee.tc@gmail.com>
2016-05-25 21:19:08 -0700

    changes to nav

--- js/Nav.js
+++ js/Nav.js
@@ -1,26 +1,27 @@
+function bindSearchFormHandlers() {
+  $("#search-field").submit(function(e){
+    e.preventDefault();
+    console.log("dsfdssdf");
+    //navigate to search page,
+    //if on search page, change/update the results
+  });
+};
+
 function setUpNavBar() {
-  var $searchEndDate = $("#search-end-date");
-  var $searchField = $("#search-field");
-  var $searchText = $("#search-text");
-  var $searchDate = $("#search-start-date");
-
   var $searchBtn = $("#searchBtn");
-  var searchMenuContent = $("#searchMenu").html();
+  var $searchEndDate = $("#search-end-date");
 
+  var d = new Date(Date.now());
+  $searchEndDate.attr("placeholder", d.getMonth() + "/" + d.getDay() + "/" + d.getFullYear());
+
+  var searchMenuContent = $("#searchMenu").html();
+  $("#searchMenu").remove();
   $searchBtn.popover({
     html: true,
     content: searchMenuContent
   });
 
-
-  var d = new Date(Date.now());
-  $searchEndDate.attr("placeholder", d.getMonth() + "/" + d.getDay() + "/" + d.getFullYear());
-
-  $searchField.submit(function(e){
-    e.preventDefault();
-    //navigate to search page,
-    //if on search page, change/update the results
-  });
+  $searchBtn.on('shown.bs.popover', bindSearchFormHandlers);
 }
 
 $(function(){
